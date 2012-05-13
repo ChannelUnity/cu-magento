c18f13ac2794d7a298b08100ec91465002c5b8da
Matt Ross <matt@camiloo.co.uk>
2012-05-13 23:25:19 +0000

    Override product collection

---
+++ code/community/Camiloo/Channelunity/Model/Collection.php
@@ -0,0 +1,11 @@
+<?php
+    
+    
+    class Camiloo_Channelunity_Model_Collection extends Mage_Catalog_Model_Resource_Product_Collection {
+        public function isEnabledFlat()
+        {
+            return false;
+        }
+    }
+    
+    
